82cfcede519b90a1cb28feac062a80edbdfa7fa1
jklq <mail@seang.xyz>
2021-03-10 20:06:28 +0100

    feat(profile): endpoint for setting your role

--- api/des/user/db_actions.py
+++ api/des/user/db_actions.py
@@ -5,6 +5,8 @@ from .helpers.hash_helper import hash_password
 def user_exists(**user):
     if not 'username' in user:
         user_to_find = User.query.filter_by(email=user["email"]).first() 
+    elif not 'email' in user:
+        user_to_find = User.query.filter_by(username=user["username"]).first() 
     else:
         user_to_find = User.query.filter((User.username==user["username"])
                                          | (User.email==user['email'])).first()
@@ -29,4 +31,24 @@ def create_user(**user):
     else:
         return {'msg': 'User Already Exists', 'status': 409}
 
+def update_user_role(username, role, allowOverwrite=False):
+    user_to_update_exist = user_exists(username=username)
 
+    if not user_to_update_exist['result']:
+        return {'msg': 'User not found', 'status': 404}
+    else:
+        user_to_update = user_to_update_exist['matching_user']
+
+    if not allowOverwrite and user_to_update.role != None:
+        return {"msg": 'Role already set', 'status': 409}
+
+    if role != 'c' and role != 'b' and role != 'm':
+        return {"msg": 'Invalid role', 'status': 422}
+
+    # SOME ROLES ARE CURRENTLY NOT DEVELOPED
+    if role == 'c' or role == 'm':
+        return {"msg": 'Role currently not available', 'status': 403}
+ 
+    user_to_update.role = role
+    db.session.commit()
+    return {'msg': 'Role set', 'status': 200}
--- api/des/user/profile/__init__.py
+++ api/des/user/profile/__init__.py
@@ -1,14 +1,23 @@
-from flask import Blueprint, request
-from flask_classful import FlaskView
+from flask import request, jsonify
+from flask_classful import FlaskView, route
 from flask_jwt_extended import jwt_required, get_jwt_identity
-
-profile = Blueprint('profile', __name__)
+from ..db_actions import update_user_role
 
 class ProfileView(FlaskView):
-    route_base = '/'
-    excluded_methods = ['login_user']
-    
+
     @jwt_required()
     def index(self):
         current_user = get_jwt_identity()
         return current_user
+    
+    @jwt_required()
+    @route("/select-role/" ,methods=["POST"])
+    def select_role(self):
+        role = request.json.get('role')
+        username = get_jwt_identity()
+        response = update_user_role(username, role)
+        resMsg = response['msg']
+        status = response['status']
+        return jsonify({'msg': resMsg}), status
+
+
