f8a61256660111b10094c90ec89d9887cd26565e
jklq <mail@seang.xyz>
2021-03-14 18:13:07 +0100

    feat(user): added can-play endpoint

--- api/config.py
+++ api/config.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///../test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
 JWT_SECRET_KEY="1akljsdfjklsdfjhasdkljfhjasdkfjlsdahfkjlsad"
 JWT_ACCESS_TOKEN_EXPIRES=timedelta(days=3)
 CORS_SUPPORTS_CREDENTIALS=True
--- api/des/user/profile/__init__.py
+++ api/des/user/profile/__init__.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 from flask_classful import FlaskView, route
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from ..db_actions import update_user_role
+from ..db_actions import update_user_role, user_exists
 
 class ProfileView(FlaskView):
 
@@ -19,5 +19,19 @@ class ProfileView(FlaskView):
         resMsg = response['msg']
         status = response['status']
         return jsonify({'msg': resMsg}), status
+
+    @jwt_required()
+    @route("/can-play/" ,methods=["GET"])
+    def can_play(self):
+        username = get_jwt_identity()
+        response = user_exists(username=username)
+        if response['result']:
+            userRecord = response['matching_user']
+            if userRecord.role == None:
+                return jsonify({'msg': 'No role set', 'result': False, 'redirectUrl': '/game/select-role'}), 200
+            else:
+                return jsonify({'msg': 'Can play', 'result': True}), 200
+        else:
+            return jsonify({'msg': 'User not found'}), 404 
 
 
